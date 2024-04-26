69f724968d2b5ef669d41af37fc52ac491bab312
snehap08 <112794383+snehap08@users.noreply.github.com>
2024-04-26 23:45:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Your Website</title>
+     <!-- font awesome cdn link  -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <!-- custom cursors  -->
+<div class="cursor-1"></div>
+<div class="cursor-2"></div>
+<header>
+    <nav>
+      <div class="logo-container">
+        <div class="logo">
+          <!-- Replace the src attribute with the path to your logo image -->
+          <img
+            src="https://cdn.dorik.com/64fd87d5660c540011034d97/64fd8993660c540011035308/images/H-removebg-preview_48o4txqt.png"
+            alt="Logo"
+          />
+        </div>
+        <div class="logo-text">Sneha Pandey</div>
+      </div>
+      <div class="menu-icon" onclick="toggleMenu">&#9776;</div>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#internship">Internship</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      
+      <div class="social-icons">
+        <li>
+            <a href="https://github.com/snehap08"><i class="fab fa-github"></i></a>
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/in/sneha-pandey-05a970223/"><i class="fab fa-linkedin"></i></a>
+        </li>
+        <li>
+            <a href="https://wa.me/918076060549 "><i class="fab fa-whatsapp"></i></a>
+        </li>
+      </div>
+  </ul>
+      
+    </nav>
+
+  </header>
+
+    <section id="home">
+      <!-- Video element for the background -->
+      <img src="sand-7468945.jpg" alt="John Doe">
+
+
+
+    
+      <div class="content">
+        <h2>Sneha <br> Pandey</h2>
+        <p>UI/UX Designer & Backend Developer!</p>
+      </div>
+
+      
+    </section>
+
+    
+    <section class="about" id="about">
+      <h1 class="heading">about <span>me</span></h1>
+      <br>
+      <img class="shape" src="https://cdn.dorik.com/64fd87d5660c540011034d97/64fd8993660c540011035308/images/image-removebg_en10qevl.png" alt="">
+        
+      <div class="card">
+      <div class="glassmorphism-card">
+        <h3 class="card-title">| 2003-Present |</h3>
+        
+        
+          <div class="content"></div>
+          <p>A 19-yr old female exploring the digital canvas, I am a passionate creator, weaving innovation into every pixel and line of code. A seeker of beauty in design, a solver of problems in code, and an endless dreamer in the realm of possibilities. With each keystroke, I paint my unique story on the canvas of the web, shaping the future one pixel at a time.</p>
+
+          <a href="Sneha Pandey.pdf" class="btn">Know More</a>
+      <a href="https://www.linkedin.com/in/sneha-pandey-05a970223/details/certifications/" class="btn">Certfolio</a>
+        </div>
+      </div>
+      
+
+    </div>  
+    </section>
+    
+    <section id="skills">
+      <h1 class="heading">My <span>Skills</span></h1>
+        <br>
+      <div class="container">
+        <div class="person-container">
+          <img src="https://ouch-cdn2.icons8.com/r6YFsSVzRRzXFnglRAaboyQ_pzF5UBsTCDM_KIdi71w/rs:fit:368:466/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNTI1/LzFkMDI2MmIwLWM3/ZDItNDcyNS1iMTM5/LWRmZmE1YTE0ZDc4/MS5wbmc.png" alt="Person" class="person">
+          <div class="skill-icons">
+            <div class="skill-icon">
+              <img src="https://static.vecteezy.com/system/resources/previews/009/992/271/non_2x/ui-ux-icon-sign-design-free-png.png" alt="HTML1" class="img1">
+              <span class="skill-label"></span>
+            </div> <!-- JAVASCRIPT -->
+            
+            <div class="skill-icon">
+              <img src="https://juststickers.in/wp-content/uploads/2016/05/django-badge.png" alt="HTML3" class="img3">
+              <span class="skill-label"></span>
+            </div> <!-- DJANGO -->
+            <div class="skill-icon">
+              <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_wordmark_logo_icon_146425.png"class="img4">
+              <span class="skill-label"></span>
+            </div> <!-- UX-->
+            <div class="skill-icon">
+              <img src="https://www.pngall.com/wp-content/uploads/13/Figma-Logo-PNG-Photos.png" alt="HTML4"class="img5">
+              <span class="skill-label"></span>
+              <!-- UI -->
+            </div> 
+            <div class="skill-icon">
+              <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/canva-icon.png" alt="HTML4"class="img6">
+              <span class="skill-label"></span>
+            </div> <!-- CSS icon, add more as needed -->
+            <div class="skill-icon">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2560px-Node.js_logo.svg.png" alt="HTML6"class="img7">
+              <span class="skill-label"></span>
+            </div>
+            <!-- Add more skill icons here -->
+          </div>
+        </div>     
+      </div>
+     
+    </section>
+    
+
+    <section id="internship" class="internship-section">
+      
+        <h1 class="heading">Intern<span>ships</span></h1>
+        <br><div class="content">
+        <p class="p1">Recent work & Achievements</p>
+        <h2>Sync Intern's: UI/UX Designer intern</h2> <div class="image">
+          <img src="https://static.wixstatic.com/media/5d3a75_a74999aa4fc34c90a7e45de2b3dc9b2a~mv2.png/v1/fill/w_228,h_102,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/IMG_20230706_224542.png" alt="UI/UX design image"/>
+        </div>
+        <p class="p2">On 15 July 2023</p><br>
+        <p class="p3">"I previously worked as a UI/UX Designer Intern at Sync Intern's. During my time there, I had the chance to work on various projects that provided me with valuable hands-on experience in this field. It was a great learning experience, and I'm thankful for the opportunities it offered in my journey as a UI/UX designer."</p><br>
+        
+
+
+        <p class="p4"><hr class="vertical-line"> &nbsp;“Design is not just what it looks like and feels like. Design is how it works.” - Steve Jobs, the co-founder of Apple Inc.</p>
+        <div class="buttons">
+          <a href="https://www.linkedin.com/in/sneha-pandey-05a970223/recent-activity/videos/" class="btn">View Project</a>
+          <a href="https://drive.google.com/file/d/1wt9Jf4pUBoVZwIGdCXtaNF8p_0NiSSwU/view?usp=drive_link" class="btn">Letter of Recommendation</a>
+        </div>
+      </div>
+      
+    </section>
+
+    <section id="internship2" class="internship-section">
+  
+      <br><div class="content">
+      <h2>Prodigy Infotech: Web Development intern </h2> <div class="image">
+        <img src="https://media.cakeresume.com/image/upload/s--SCNj-FKY--/c_pad,fl_png8,h_400,w_400/v1694524949/omeei5nqdfncqshwzr8q.png" alt="UI/UX design image"/>
+      </div>
+      <p class="p2">On 23 August 2023</p><br>
+      <p class="p3">"I'm excited to join the Prodigy Infotech team as a Web Development Intern. Previously, I gained practical experience as a UI/UX Designer Intern at Sync Intern's, where I honed my design skills and fostered a passion for creating seamless user experiences. Now, I'm eager to channel that passion into web development and contribute to the innovative work being done here at Prodigy."</p><br>
+      <div class="buttons">
+        <a href="https://github.com/snehap08/To-do_List" class="btn">View Project</a>
+      </div>
+    </div>
+    
+  </section>
+    
+
+    <section id="projects">
+      <h1 class="heading">Pro<span>jects</span></h1>
+        <br><div class="content">
+        <p class="p1">Some personal projects</p>
+        <h2>E-commerce website with Django Framework</h2> <div class="image">
+          <img src="https://marketplace.canva.com/EAE72rd_his/2/0/1600w/canva-yellow-e-commerce-shop-bag-store-logo-hxc1-hr_SQA.jpg" alt="UI/UX design image"/>
+        </div>
+        <p class="p3">"At the heart of our e-commerce website lies Django, the driving force behind a seamless and secure shopping experience. Our user-friendly platform makes browsing, selecting, and purchasing products a breeze. With robust features, efficient order processing, and uncompromising security, we're committed to providing a top-notch e-commerce destination for our customers. Explore our store, discover your favorites, and embark on a shopping journey that's as convenient as it is enjoyable."</p><br>
+        
+
+        <div class="buttons">
+          <a href="https://github.com/snehap08/E-commerce_website" class="btn">View Project</a>
+         
+        </div>
+      </div>
+    </section>
+
+    <section id="projects2" class="projects-section">
+  
+      <br><div class="content">
+      <h2>Web Development with Javascript</h2> <div class="image">
+        <img src="https://cdn.dorik.com/64fd87d5660c540011034d97/64fd8993660c540011035308/images/Screenshot-2023-10-03-224133_gsztu8ah.png" alt="UI/UX design image"/>
+      </div>
+      <p class="p3">With the advancement in time, i came across various beautiful languages to design web pages. Among all i chose Django and Javascript to work with. Here below are the link of my projects showcasing my Django skills through an "E-commerce website" and my Javascript skills through an "Animated locomotive website".
+      </p><br>
+      <div class="buttons">
+        <a href="https://github.com/snehap08/Sherlock_animation" class="btn">View Project</a>
+      </div>
+    </div>
+    
+  </section>
+
+  <section id="projects">
+        <br><div class="content">
+        <p class="p1">Some personal projects</p>
+        <h2>LEAF Chatbot using Python and Gemini</h2> <div class="image">
+          <img src="https://go4customer.com/images/blog/SIMPLE%20CHATBOTS.png" alt="chatbot image"/>
+        </div>
+        <p class="p3">"The project L-E-A-F(Learn English with AI Fluently) is not only a language learning app but also a transformative business idea to revolutionize the language learning experience. With the integration of Artificial intelligence, our platform seamlessly delivers an immersive language learning experience to the users that feels much better than the traditional learning methods by providing personalized feedback, innovative features, gamified motivation, and many more. Our project caters to a global audience, addressing the importance of effective communication. L-E-A-F is not only about learning English, it is also about developing an evolving and sustainable solution for the individuals who are seeking for improvement in their daily communication skills."</p><br>
+        <p style="color: red; font-size: 16px;">Note: This project is still in progress</p>
+        
+
+        <div class="buttons">
+          <a href="https://github.com/snehap08/LEAF_GEMINI" class="btn">View Project</a>
+         
+        </div>
+      </div>
+    </section>
+
+  <section class="contact" id="contact">
+
+    <h1 class="heading"> contact <span> me </span> </h1>
+
+    <div class="row">
+
+        <form action="" method="post">
+
+            <input type="text" name="name" placeholder="Name" class="box">
+            <input type="email" name="email" placeholder="Email" class="box">
+            <input type="number" name="phone" placeholder="Phone Number" class="box">
+
+            <textarea name="message" placeholder="Message" id="" cols="30" rows="10"></textarea>
+
+            <input type="submit" class="btn" name="submit" value="Send Message">
+            
+      
+
+        </form>
+    </div>
+</section>
+
+<?php
+
+    //Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+if  (isset($_POST['submit'])) {
+  $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+
+
+
+
+//Load Composer's autoloader
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'sneha.pandey1208@gmail.com';                     //SMTP username
+    $mail->Password   = 'vozn gaht kndr jmkl';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('sneha.pandey1208@gmail.com', 'snehaportfolio');
+    $mail->addAddress('sneha.cloud1208@gmail.com', 'Joe User');     //Add a recipient
+
+    
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = "Sender Name - $name <br> Sender Email - $email <br> Message - $message <br>";
+    
+
+    $mail->send();
+    echo "<div class ='success'> Message has been sent </div>";
+} catch (Exception $e) {
+    echo "<div class ='alert'> Message could not sent Error: {$mail->ErrorInfo} </div>";
+}
+}
+?>
+
+<!-- footer section  -->
+<footer class="footer">created by <span> Sneha Pandey</span> | all rights reserved! </footer>
+
+
+    <script>
+
+      // JavaScript for toggling menu on small screens
+      document.querySelector('.menu-icon').addEventListener('click', function() {
+          const navMenu = document.querySelector("nav ul");
+          navMenu.classList.toggle("show");
+      });
+
+
+      // Smooth scrolling for anchor links
+      document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+          });
+
+          // Close the menu on click (for small screens)
+          const navMenu = document.querySelector("nav ul");
+          navMenu.classList.remove("show");
+        });
+      });
+
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) =>{
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
+document.querySelectorAll('a').forEach(links =>{
+
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+
+});
+  
+    </script>
+  </body>
+</html>
+
+
+
+
+
