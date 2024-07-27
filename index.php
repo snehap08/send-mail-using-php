7e955a63f6fb07e29e66adf0778a83fddb6e188c
snehap08 <112794383+snehap08@users.noreply.github.com>
2024-07-27 21:45:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -63,14 +63,14 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'sneha.pandey1208@gmail.com';                     //SMTP username
-    $mail->Password   = 'vmov exxe uady qvry';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('sneha.pandey1208@gmail.com', 'snehaportfolio');
-    $mail->addAddress('sneha.cloud1208@gmail.com', 'Joe User');     //Add a recipient
+    $mail->setFrom('', '');
+    $mail->addAddress('', '');     //Add a recipient
 
     
 
